f57d8d8f6792c73bdff67aa2d7739b365ed10aa8
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-05-02 00:48:43 -0400

    Modelo Trabajo
    Esquema y Modelo de Trabajo

---
+++ models/Trabajo.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const trabajoSchema = new mongoose.Schema({
+  tema: {
+    type: String,
+    required: true,
+  },
+  materia: {
+    type: String,
+    required: true,
+  },
+  tipo: {
+    type: String,
+    required: true,
+  },
+  cliente: {
+    type: Schema.Types.ObjectId,
+    ref: "Cliente",
+    required: true,
+  },
+  profesor: {
+    type: Schema.Types.ObjectId,
+    ref: "Profesor",
+    required: true,
+  },
+  encargado: {
+    type: Schema.Types.ObjectId,
+    ref: "Encargado",
+    required: true,
+  },
+  fechaInicial: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  fechaFinal: {
+    type: Date,
+    required: true,
+  },
+  duracion: {
+    type: Number,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  pagoProfesor: {
+    type: Number,
+    required: true,
+  },
+  realizado: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Trabajo", trabajoSchema);
