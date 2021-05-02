e3c274b5037e3b4a95507a5ad909a64493b7482d
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-05-02 00:51:50 -0400

    Modelo Cliente
    Esquema y Modelo de Cliente

---
+++ models/Cliente.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const clienteSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+  },
+  cedula: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  universidad: {
+    type: String,
+    required: true,
+  },
+  carrera: {
+    type: String,
+    required: true,
+  },
+  pais: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Cliente", clienteSchema);
