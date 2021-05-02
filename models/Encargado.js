88f2ca73499d42381cf2b6772f0d99c69166b8ab
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-05-02 00:55:20 -0400

    Modelo Encargado
    Esquema y Modelo de Encargado

---
+++ models/Encargado.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const encargadoSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+  },
+  cedula: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  cargo: {
+    type: String,
+    required: true,
+  },
+  pais: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Encargado", encargadoSchema);
